409df4feff92fa17d8cf630c5a88d753e47c8ebe
Ryan Pavlik <rpavlik@gmail.com>
2018-06-13 15:03:57 -0500

    Actually templatize rect

--- include/gk/rect.hpp
+++ include/gk/rect.hpp
@@ -6,12 +6,14 @@ namespace gk {
 
 template<typename T, typename F>
 struct GK_CORE_CXX_API trect {
+    using vec2 = tvec2<T,F>;
+
     vec2 pos;
     vec2 size;
 
     trect() = default;
     trect(const trect &r) { pos = r.pos; size = r.size; }
-    trect(float x, float y, float w, float h)
+    trect(T x, T y, T w, T h)
         : pos(x, y), size(w, h) {
     }
     trect(const vec2 &pos_, const vec2 &size_)
@@ -20,7 +22,13 @@ struct GK_CORE_CXX_API trect {
 
     trect& operator=(const trect &r) { pos = r.pos; size = r.size; return *this; }
 
-    inline gk::vec2 v1abs() const { return gk::vec2(pos.x + size.x, pos.y + size.y); }
+    template<typename U, typename V>
+    trect operator+(const tvec2<U,V> &v) const { return trect{pos + v, size}; }
+
+    template<typename U, typename V>
+    trect operator-(const tvec2<U,V> &v) const { return trect{pos - v, size}; }
+
+    inline vec2 v1abs() const { return vec2(pos.x + size.x, pos.y + size.y); }
 
     bool contains(const vec2 &p) const {
         return (pos.x <= p.x) && (pos.y <= p.y) &&
